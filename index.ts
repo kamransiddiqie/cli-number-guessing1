ff0efe9283c3084247955e82f8818c2b67c7ada4
kamransiddiqie <164396731+kamransiddiqie@users.noreply.github.com>
2024-04-05 11:13:58 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+import inquirer from "inquirer";
+
+const randomNubber = Math.floor(Math.random() * 10 +1);
+const answers = await inquirer.prompt([{
+    name: "userGuessnumber",
+    type: "number",
+    message: "please guess a number:"
+
+}])
+if(answers.userGuessnumber === randomNubber){
+    console.log("congrutulation you guessed right number.")
+    
+}
+else(console.log("you guessed wrong nnmber."));
